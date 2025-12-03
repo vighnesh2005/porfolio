e49d1dc3d6f48589a04d000208d4a597115e3814
vighnesh2005 <vighneshprasad12@gmail.com>
2025-12-03 21:47:45 +0530

    added coding stats

--- app/page.jsx
+++ app/page.jsx
@@ -3,19 +3,21 @@ import Hero from "@/sections/hero";
 import Skills from "@/sections/Skills";
 import Projects from "@/sections/projects";
 import Education from "@/sections/education";
+import CodingStats from "@/sections/CodingStats";
 import Contact from "@/sections/contact";
 
 export default function Home() {
   return (
     <>
-    <Navbar /> 
-    <div className="flex flex-col gap-30">
-    <Hero />
-    <Education />
-    <Skills />
-    <Projects />
-    <Contact />
-    </div>
+      <Navbar />
+      <div className="flex flex-col gap-30">
+        <Hero />
+        <Education />
+        <Skills />
+        <Projects />
+        <CodingStats />
+        <Contact />
+      </div>
     </>
   );
 }
